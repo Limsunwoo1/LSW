afe3d405c0b727a48da377e8061a0439eaeef373
Limsunwoo1 <88051962+Limsunwoo1@users.noreply.github.com>
2022-09-07 17:51:43 +0900

    2022 09 07

---
+++ 06. function/06. function.cpp
@@ -0,0 +1,36 @@
+﻿#include <iostream>
+
+int main()
+{
+    // 함수
+
+// 반환값 함수 이름( 전달 인자 ) 
+
+/*ex) void FuntionA()
+{
+   return 0;
+}*/
+
+// 함수 오버로딩
+// 같은 이름의 함수도 전달인자의 종류에따라서
+// 선언할수 있도록 가능하게 해주는 기능
+
+    void Print()
+    {
+
+    }
+    void Print(int A)
+    {
+
+    }
+
+    //반환값
+    int add(int a, int b)
+    {
+        int result = a + b;
+        return ressult;
+        // return 값은 반환형과 일치해야한다
+        // 지역변수는 함수 종료화함께 메모리 반환
+    }
+}
+
---
+++ 07. 배열/07. 배열.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+
+int main()
+{
+    //배열  
+// 배열로 생성시 같은위치에 랜덤생성
+    int arr[4] = { 1, 2, 3, 4 }; //배열초기화 방식
+
+    // 배열 크기이상 접근하는거 주의
+
+    arr[0], arr[1], arr[2], arr[3], <arr[4]>
+
+
+        // 글자가 여러개인것 문자열
+        // 배열 끝자리에는 \0가 생략되어있다
+        // 컴퓨터는 글자와 문자열을 구분하지못한다.
+        char str[256] = "ABCE\0";
+
+    //  얕은복사 call by value
+    //  깊은복사 call by reference
+
+    // 구조체
+// 사용자 정의 자료형
+
+    stuct Player
+    {
+       char name[256];
+       char Class[256];
+       int hp;
+       int mp;
+    }
+
+    Player player[10]; // 선언
+// 변수명 + . 으로 사용
+    player.hp = 50;
+    player.mp = 50;
+
+        // 구조체, 배열 초기화
+    Player player = {};
+    int arr[50] = {};
+
+    // 간단히 설명하면 이름 바꾸기
+    // 자료형 재정의
+    // typedef 타입 변수명
+
+    typedef struct A;
+}
